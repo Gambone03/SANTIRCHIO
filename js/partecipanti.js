639496c34e32b1d57c37e1141a4f140d3a5498be
Gambone03 <gabry.rigatuso03@gmail.com>
2026-08-05 19:00:06 +0200

    nuovo partecipanti

--- js/partecipanti.js
+++ js/partecipanti.js
@@ -3,61 +3,101 @@ const partecipanti = [
 {
     nome: "Gabriele",
     squadra: "...",
-    foto: "logo gabriele.jpeg"
+    foto: "logo gabriele.jpeg",
+    descrizione: ".",
+    rosa: [
+        "portiere"
+    ]
 },
 
 {
     nome: "Erica",
     squadra: "...",
-    foto: "logo erica.jpeg"
+    foto: "logo erica.jpeg",
+    descrizione: ".",
+    rosa: [
+        "portiere"
+    ]
 },
 
 {
     nome: "Santi",
     squadra: "...",
-    foto: "logo santi.jpeg"
+    foto: "logo santi.jpeg",
+    descrizione: ".",
+    rosa: [
+        "portiere"
+    ]
 },
 
 {
     nome: "Alessandro",
     squadra: "...",
-    foto: "logo alessandro.jpeg"
+    foto: "logo alessandro.jpeg",
+    descrizione: ".",
+    rosa: [
+        "portiere"
+    ]
 },
 
 {
     nome: "Santino",
     squadra: "...",
-    foto: "logo santino.jpeg"
+    foto: "logo santino.jpeg",
+    descrizione: ".",
+    rosa: [
+        "portiere"
+    ]
 },
 
 {
     nome: "Piero",
     squadra: "...",
-    foto: "logo piero.jpeg"
+    foto: "logo piero.jpeg",
+    descrizione: ".",
+    rosa: [
+        "portiere"
+    ]
 },
 
 {
     nome: "Antonio",
     squadra: "TOTERAY HOTSPUR",
-    foto: "logo antonio.jpeg"
+    foto: "logo antonio.jpeg",
+    descrizione: ".",
+    rosa: [
+        "portiere"
+    ]
 },
 
 {
     nome: "Christian",
     squadra: "...",
-    foto: "logo christian.jpeg"
+    foto: "logo christian.jpeg",
+    descrizione: ".",
+    rosa: [
+        "portiere"
+    ]
 },
 
 {
     nome: "Mario",
     squadra: "...",
-    foto: "logo mario.jpeg"
+    foto: "logo mario.jpeg",
+    descrizione: ".",
+    rosa: [
+        "portiere"
+    ]
 },
 
 {
     nome: "Emanuela",
     squadra: "...",
-    foto: "logo emanuela.jpeg"
+    foto: "logo emanuela.jpeg",
+    descrizione: ".",
+    rosa: [
+        "portiere"
+    ]
 }
 
 ];
