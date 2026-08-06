d192eeaa47a4ec1275a0629179453dee7a43340d
Gambone03 <gabry.rigatuso03@gmail.com>
2026-08-06 23:33:30 +0200

    s

--- js/mvp.js
+++ js/mvp.js
@@ -22,6 +22,210 @@ const mvp = [
     giornata: 4,
     vincitore: "",
     squadra: "-"
+},
+
+{
+    giornata: 5,
+    vincitore: "",
+    squadra: "-"
+},
+
+{
+    giornata: 6,
+    vincitore: "",
+    squadra: "-"
+},
+
+{
+    giornata: 7,
+    vincitore: "",
+    squadra: "-"
+},
+
+{
+    giornata: 8,
+    vincitore: "",
+    squadra: "-"
+},
+
+{
+    giornata: 9,
+    vincitore: "",
+    squadra: "-"
+},
+
+{
+    giornata: 10,
+    vincitore: "",
+    squadra: "-"
+},
+
+{
+    giornata: 11,
+    vincitore: "",
+    squadra: "-"
+},
+
+{
+    giornata: 12,
+    vincitore: "",
+    squadra: "-"
+},
+
+{
+    giornata: 13,
+    vincitore: "",
+    squadra: "-"
+},
+
+{
+    giornata: 14,
+    vincitore: "",
+    squadra: "-"
+},
+
+{
+    giornata: 15,
+    vincitore: "",
+    squadra: "-"
+},
+
+{
+    giornata: 16,
+    vincitore: "",
+    squadra: "-"
+},
+
+{
+    giornata: 17,
+    vincitore: "",
+    squadra: "-"
+},
+
+{
+    giornata: 18,
+    vincitore: "",
+    squadra: "-"
+},
+
+{
+    giornata: 19,
+    vincitore: "",
+    squadra: "-"
+},
+
+{
+    giornata: 20,
+    vincitore: "",
+    squadra: "-"
+},
+
+{
+    giornata: 21,
+    vincitore: "",
+    squadra: "-"
+},
+
+{
+    giornata: 22,
+    vincitore: "",
+    squadra: "-"
+},
+
+{
+    giornata: 23,
+    vincitore: "",
+    squadra: "-"
+},
+
+{
+    giornata: 24,
+    vincitore: "",
+    squadra: "-"
+},
+
+{
+    giornata: 25,
+    vincitore: "",
+    squadra: "-"
+},
+
+{
+    giornata: 26,
+    vincitore: "",
+    squadra: "-"
+},
+
+{
+    giornata: 27,
+    vincitore: "",
+    squadra: "-"
+},
+
+{
+    giornata: 28,
+    vincitore: "",
+    squadra: "-"
+},
+
+{
+    giornata: 29,
+    vincitore: "",
+    squadra: "-"
+},
+
+{
+    giornata: 30,
+    vincitore: "",
+    squadra: "-"
+},
+
+{
+    giornata: 31,
+    vincitore: "",
+    squadra: "-"
+},
+
+{
+    giornata: 32,
+    vincitore: "",
+    squadra: "-"
+},
+
+{
+    giornata: 33,
+    vincitore: "",
+    squadra: "-"
+},
+
+{
+    giornata: 34,
+    vincitore: "",
+    squadra: "-"
+},
+
+{
+    giornata: 35,
+    vincitore: "",
+    squadra: "-"
+},
+
+{
+    giornata: 36,
+    vincitore: "",
+    squadra: "-"
+},
+
+{
+    giornata: 37,
+    vincitore: "",
+    squadra: "-"
+},
+
+{
+    giornata: 38,
+    vincitore: "",
+    squadra: "-"
 }
 
 ];
