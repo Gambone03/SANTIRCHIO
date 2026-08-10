5f031073d19a702f4e896c367d946a251a46ce4e
Gambone03 <gabry.rigatuso03@gmail.com>
2026-08-10 16:01:20 +0200

    si

--- js/partecipanti.js
+++ js/partecipanti.js
@@ -16,6 +16,7 @@ const partecipanti = [
     nome: "Erica",
     squadra: "STUDIO LEGALE BAMBI",
     foto: "logo erica.jpeg",
+    nomeExtra: "LA TIFOSA",
     immagine: "erica.png",
     descrizione: "Per qualsiasi ricorso sportivo e non ci trovate tra le Alpi e gli Appennini",
     rosa: [
@@ -27,6 +28,7 @@ const partecipanti = [
     nome: "Santi",
     squadra: "REAL BETONIERA",
     foto: "logo santi.jpeg",
+    nomeExtra: "IL TALENT SCOUT",
     immagine: "santi.jpeg",
     descrizione: "Dateci del calcestruzzo e costruiremo il nostro futuro, senza ci proviamo lo stesso",
     rosa: [
@@ -38,6 +40,7 @@ const partecipanti = [
     nome: "Alessandro",
     squadra: "REAL CALARU I FRANE",
     foto: "logo alessandro.jpeg",
+    nomeExtra: "L'ALZATORE",
     immagine: "alessandro.png",
     descrizione: "Allenamenti personalizzati: <strong>Pala, sciamarru e corsetta alle 4 del mattino<strong>",
     rosa: [
@@ -49,6 +52,7 @@ const partecipanti = [
     nome: "Santino",
     squadra: "YOUNG GIRLS",
     foto: "logo santino.jpeg",
+    nomeExtra: "IL PIANGINA",
     immagine: "santino.png",
     descrizione: "Vivaio, giovani promesse, talenti da scoprire...in poche parole <strong>CBCR<strong>",
     rosa: [
@@ -60,6 +64,7 @@ const partecipanti = [
     nome: "Piero",
     squadra: "LIVERPUZZ",
     foto: "logo piero.jpeg",
+    nomeExtra: "IL PROFESSORE",
     immagine: "piero.png",
     descrizione: "You'll never PUZZ alone, vi conviene starci lontani",
     rosa: [
@@ -71,6 +76,7 @@ const partecipanti = [
     nome: "Antonio",
     squadra: "TOTERAY HOTSPUR",
     foto: "logo antonio.jpeg",
+    nomeExtra: "IL VISIONARIO",
     immagine: "antonio.png",
     descrizione: "Una storia con radici nel passato e con cassetto pieno zeppo di sogni.<br><strong>Sognare è sempre stato un nostro diritto<strong>",
     rosa: [
@@ -82,6 +88,7 @@ const partecipanti = [
     nome: "Christian",
     squadra: "GIOCO D'HAZARD",
     foto: "logo christian.jpeg",
+    nomeExtra: "LO SFIGATO",
     immagine: "christian.png",
     descrizione: "Chi non risica non rosica e la paura è un sentimento che appartiene ai deboli, gls",
     rosa: [
@@ -91,10 +98,11 @@ const partecipanti = [
 
 {
     nome: "Mario",
-    squadra: "...",
+    squadra: "-",
     foto: "logo mario.jpeg",
+    nomeExtra: "-",
     immagine: "mario.jpeg",
-    descrizione: ".",
+    descrizione: "-",
     rosa: [
         "portiere"
     ]
@@ -104,6 +112,7 @@ const partecipanti = [
     nome: "Emanuela",
     squadra: "AC QUOTA ROSA",
     foto: "logo emanuela.png",
+    nomeExtra: "-",
     immagine: "emanuela.png",
     descrizione: "Il calcio non è uno sport per femminucce ma per <strong>donne di un certo calibro<strong>",
     rosa: [
