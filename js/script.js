e479b4f41a30533e79fb3e6f43a5049cd172eb79
Gambone03 <gabry.rigatuso03@gmail.com>
2026-08-17 15:45:53 +0200

    s

--- js/script.js
+++ js/script.js
@@ -164,8 +164,6 @@ function apriCoppa(coppa) {
         immagineA.src = "img/coppe/coppa1-girone-a.png";
         immagineB.src = "img/coppe/coppa1-girone-b.png";
         faseFinale.src = "img/coppe/coppa1-fase-finale.png";
-
-        faseFinale.style.display = "block";
     
     }
 
